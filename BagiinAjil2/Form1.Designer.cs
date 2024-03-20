93dde7387a8eba4ed2c076367e69b58bc77c2d9c
Temuge <temuge.erdene@gmail.com>
2024-03-20 12:09:19 +0800

    Add project files.

---
+++ BagiinAjil2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace BagiinAjil2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            fromDate = new DateTimePicker();
+            toDate = new DateTimePicker();
+            button1 = new Button();
+            differenceText = new RichTextBox();
+            SuspendLayout();
+            // 
+            // fromDate
+            // 
+            fromDate.Location = new Point(68, 63);
+            fromDate.Name = "fromDate";
+            fromDate.Size = new Size(200, 23);
+            fromDate.TabIndex = 0;
+            // 
+            // toDate
+            // 
+            toDate.Location = new Point(454, 63);
+            toDate.Name = "toDate";
+            toDate.Size = new Size(200, 23);
+            toDate.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(321, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // differenceText
+            // 
+            differenceText.Location = new Point(217, 209);
+            differenceText.Name = "differenceText";
+            differenceText.Size = new Size(402, 187);
+            differenceText.TabIndex = 4;
+            differenceText.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(differenceText);
+            Controls.Add(button1);
+            Controls.Add(toDate);
+            Controls.Add(fromDate);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker fromDate;
+        private DateTimePicker toDate;
+        private Button button1;
+        private RichTextBox differenceText;
+    }
+}
---
+++ BagiinAjil2/Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace BagiinAjil2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateDifferenceDate dateDifferenceData = new DateDifferenceDate(fromDate.Value, toDate.Value);
+            differenceText.Text = dateDifferenceData.ToString();
+        }
+    }
+    public class DateDifferenceDate
+    {
+        public int Years;
+        public int Months;
+        public int Days;
+        public int Hours;
+        public int Minutes;
+        public long Seconds;
+        public int Week;
+        public int Test;
+        
+        public DateDifferenceDate(DateTime fromDateValue, DateTime toDateValue)
+        {
+            var difference = toDateValue - fromDateValue;
+            Days = (int)difference.TotalDays;
+            Minutes = (int)difference.TotalMinutes;
+            Seconds = (long)difference.TotalSeconds;
+            Hours = (int)difference.TotalHours;
+            Years = Days / 365;
+            Months = Days / 30;
+            Week = Days / 7;
+        }
+        public override string ToString()
+        {
+            return $"{Years} Жил {Environment.NewLine} {Months} Сар {Environment.NewLine} {Days} Өдөр {Environment.NewLine}" +
+                $"{Hours} Цаг {Environment.NewLine} {Minutes} Минут {Environment.NewLine}" +
+                $"{Seconds} Секунд {Environment.NewLine}" +
+                $"{Week} 7 хоног";
+        }
+    }
+}
